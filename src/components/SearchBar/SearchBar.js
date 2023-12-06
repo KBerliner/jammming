d320d788e9ea31edc11d934aa1565643e198e9ef
Kyle <berlinerkyle@gmail.com>
2023-12-06 13:33:59 -0700

    Completed the Search Bar. I used a Material Icon for the Search Icon.

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import styles from './SearchBar.module.css';
+import NewSearchIcon from '../../visual/search_FILL0_wght400_GRAD0_opsz24.svg';
+
+export default function SearchBar() {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    function handleInput({ target }) {
+        setSearchQuery(target.value);
+    }
+
+    return (
+        <div>
+            <form>
+                <input className={styles.neumorphismShadow} onChange={handleInput} value={searchQuery} type="text" />
+                <img src={NewSearchIcon} className={`${styles.searchIcon}`} />
+            </form>
+        </div>
+    )
+}
