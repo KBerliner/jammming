3db773368ffcca67cb4c9336a5ae550b83874297
Kyle <berlinerkyle@gmail.com>
2023-12-06 22:21:57 -0700

    Fixed the tracks design, and added the Add To Playlist button.

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Track.module.css';
+import addBtn from '../../visual/add_FILL0_wght400_GRAD0_opsz24.svg';
 
 export default function Track(props) {
     return (
@@ -7,6 +8,7 @@ export default function Track(props) {
             <h3>{props.name}</h3>
             <h4>{props.artist}</h4>
             <h4>{props.album}</h4>
+            <img className={styles.addToPlaylistButton} src={addBtn} />
         </div>
     )
 }
